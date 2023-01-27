d7a2aa166290db74c43c2a20f4d575d1f3a8da23
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-01-27 01:56:17 +0100

    Fix 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (Number.isNaN(num) || num < 0) {
+    return "ERROR";
+  }
 
+  if (num === 0) {
+    return "";
+  }
+
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
