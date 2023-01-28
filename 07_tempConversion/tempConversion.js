1d35e1321505c26ce7736b2d83cdeebd5921a176
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-01-29 00:35:12 +0100

    Fix 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const roundFloat = (num) => {
+  const newNum = Number.parseFloat(num.toFixed(2));
+  return Math.round(newNum * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  return roundFloat(((temp - 32) * 5) / 9);
+};
+
+const convertToFahrenheit = function (temp) {
+  return roundFloat((temp * 9) / 5 + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
