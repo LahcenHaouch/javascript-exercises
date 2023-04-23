454d8b68ff4944cdeecc7fa71d8cf202687c6642
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-04-24 00:02:23 +0200

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function(str) {
+  const elements = [...str.toLowerCase()].filter(char => {
+    const code = char.charCodeAt();
 
+    return (code >= 97 && code <= 122);
+  })
+
+  const length = elements.length;
+  for (let i = 0; i < Math.round(length) / 2; i++) {
+
+    if (elements[i] !== elements[length - 1 - i]) {
+      return false
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
