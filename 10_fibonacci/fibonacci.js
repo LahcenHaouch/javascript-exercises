d8631e02ec1d5674478a940140d13f373c6c8404
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-04-26 00:09:23 +0200

    Add solution to 10_fibonnacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(numP) {
+  const num = Number.parseInt(numP, 10);
 
+  if (Number.isNaN(num) || num < 0) {
+    return 'OOPS';
+  }
+
+  const fibs = [];
+  for (let i = 0; i < num; i++) {
+    if (i < 2) {
+      fibs.push(1);
+    } else {
+      fibs.push(fibs.at(-1) + fibs.at(-2));
+    }
+  }
+
+  return fibs.at(-1);
 };
 
 // Do not edit below this line
