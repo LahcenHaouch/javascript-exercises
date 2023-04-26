c11720a5e526c75dabc1827f2cdef34a78fe0fac
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-04-26 23:53:40 +0200

    Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function getYearOfDeath(yearOfDeath) {
+  return yearOfDeath ?? new Date().getFullYear();
+}
 
+const findTheOldest = function(people) {
+  return people.reduce((acc, current) => {
+    const firstPersonAge = getYearOfDeath(acc.yearOfDeath) - acc.yearOfBirth;
+    const secondPersonAge = getYearOfDeath(current.yearOfDeath) - current.yearOfBirth;
+
+    return firstPersonAge > secondPersonAge ? acc : current;
+  });
 };
 
 // Do not edit below this line
